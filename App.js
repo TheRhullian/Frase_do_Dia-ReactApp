159f9a125320d528c3d84e7d278272931e75b062
Rhullian Damião <rhullian4545@gmail.com>
2020-04-21 18:34:38 -0300

    BASE APP.JS CREATED

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import {View, Text} from 'react-native';
 
 const App = () => {
-  return {};
-}
+  return (
+    <View>
+      <Text>RHULLIAN DAMIÃO</Text>
+    </View>
+  );
+};
+
+export default App;
