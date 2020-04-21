3a7ec578f1f59c7b4a39005f86dcdaf29a8f32f4
Rhullian Damião <rhullian4545@gmail.com>
2020-04-21 20:51:39 -0300

    BUTTON CREATED

--- App.js
+++ App.js
@@ -1,37 +1,26 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Button} from 'react-native';
 
 const Estilos = {
-  estiloTexto: {
-    fontSize: 30,
-    backgroundColor: '#0850fb',
-    paddingTop: 50, //TEMOS PADDING VERTICAL TAMBÉM PARA BOTTOM E TOP
-    //paddingLeft: 30, //TEMOS PADDING HORIZONTA TAMBÉM PARA LEFT E RIGHT
-    //Shadow
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.4,
-    shadowRadius: 7,
-    //Text Formatation
-    color: '#fff',
-    fontStyle: 'italic',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    //textDecorationLine: 'line-through',
-  },
-
-  estiloTexto_2: {
-    fontSize: 30,
-    backgroundColor: '#0850fb',
-    marginTop: 100,
+  principal: {
+    paddingTop: 40,
   },
 };
 
+const botaoPressionado = () => {
+  alert('Botão Pressionado');
+};
+
 const App = () => {
+  const {principal} = Estilos;
   return (
-    <View>
-      <Text style={Estilos.estiloTexto}>RHULLIAN DAMIÃO</Text>
-      <Text style={Estilos.estiloTexto_2}>MARCOS DE SIQUEIRA</Text>
+    <View style={principal}>
+      <Button
+        onPress={botaoPressionado}
+        title="CLIQUE AQUI"
+        color="#AA5680"
+        accessibilityLabel="clique aqui"
+      />
     </View>
   );
 };
